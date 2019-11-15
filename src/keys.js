d020e7689dfd02499596149364e8a9622061677b
Moisés HR <moises.huaman@mdp.com.pe>
2019-11-15 14:36:57 -0500

    adicion img

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,8 @@
+// URI: 'mongodb://DebianMhr:yourfriend_440@ds143326.mlab.com:43326/temas-de-interes-database'
+//URI: 'mongodb://DebianMhr:yourfriend_440@ds045087.mlab.com:45087/db_mlab_sol'
+
 module.exports = {
   database: {
-    URI: 'mongodb://DebianMhr:yourfriend_440@ds143326.mlab.com:43326/temas-de-interes-database'
+    URI: 'mongodb://DebianMhr:yourfriend_440@ds033499.mlab.com:33499/db_mlab_sol'
   }
 };
